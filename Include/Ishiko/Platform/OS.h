0a5b32ece41ff0acf9def0456b82f2cd0d0ac58f
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-10-26 17:15:18 +0000

    Add test makefiles

---
+++ Include/Ishiko/Platform/OS.h
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2020 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/Platform/blob/master/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_PLATFORM_OS_H_
+#define _ISHIKO_PLATFORM_OS_H_
+
+#endif
