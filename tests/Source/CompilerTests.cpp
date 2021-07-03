86c9b8ba853e1e65e5aec7cf54a4e8c05f5e67d9
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-07-03 17:41:11 +0100

    Add weekly CI build (#28)

--- tests/Source/CompilerTests.cpp
+++ tests/Source/CompilerTests.cpp
@@ -1,7 +1,7 @@
 /*
     Copyright (c) 2020 Xavier Leclercq
     Released under the MIT License
-    See https://github.com/Ishiko-cpp/Platform/blob/master/LICENSE.txt
+    See https://github.com/ishiko-cpp/platform/blob/main/LICENSE.txt
 */
 
 #include "CompilerTests.h"
