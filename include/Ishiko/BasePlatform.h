0e88993fa38b96f0de6f2ebd6e13356b87add7fa
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-08-14 12:33:32 +0100

    Updates

--- include/Ishiko/BasePlatform.h
+++ include/Ishiko/BasePlatform.h
@@ -1,13 +1,13 @@
 /*
-    Copyright (c) 2020 Xavier Leclercq
+    Copyright (c) 2020-2021 Xavier Leclercq
     Released under the MIT License
-    See https://github.com/Ishiko-cpp/Platform/blob/master/LICENSE.txt
+    See https://github.com/ishiko-cpp/base-platform/blob/main/LICENSE.txt
 */
 
-#ifndef _ISHIKO_PLATFORM_H_
-#define _ISHIKO_PLATFORM_H_
+#ifndef _ISHIKO_BASEPLATFORM_H_
+#define _ISHIKO_BASEPLATFORM_H_
 
-#include "Platform/Compilers.h"
-#include "Platform/OS.h"
+#include "BasePlatform/Compilers.h"
+#include "BasePlatform/OS.h"
 
 #endif
