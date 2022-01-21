eaeb4feac3e6bd4a005b2b1b4993b85858554ba9
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-01-21 20:35:00 +0000

    Maintenance (#43)

--- tests/src/CompilerTests.h
+++ tests/src/CompilerTests.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2020-2021 Xavier Leclercq
+    Copyright (c) 2020-2022 Xavier Leclercq
     Released under the MIT License
     See https://github.com/ishiko-cpp/base-platform/blob/main/LICENSE.txt
 */
@@ -7,7 +7,7 @@
 #ifndef _ISHIKO_CPP_BASEPLATFORM_TESTS_COMPILERTESTS_H_
 #define _ISHIKO_CPP_BASEPLATFORM_TESTS_COMPILERTESTS_H_
 
-#include <Ishiko/Tests/Core.h>
+#include <Ishiko/Tests/Core.hpp>
 
 class CompilerTests : public Ishiko::Tests::TestSequence
 {
