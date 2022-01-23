47cc3aa202c4aa5da955a3fdcb5b39601bf652a6
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-01-23 16:19:55 +0000

    Maintenance (#45)

--- tests/src/OSTests.cpp
+++ tests/src/OSTests.cpp
@@ -1,16 +1,16 @@
 /*
-    Copyright (c) 2020-2021 Xavier Leclercq
+    Copyright (c) 2020-2022 Xavier Leclercq
     Released under the MIT License
     See https://github.com/ishiko-cpp/base-platform/blob/main/LICENSE.txt
 */
 
-#include "OSTests.h"
+#include "OSTests.hpp"
 #include "Ishiko/BasePlatform/OS.h"
 
 using namespace Ishiko::Tests;
 
-OSTests::OSTests(const TestNumber& number, const TestEnvironment& environment)
-    : TestSequence(number, "OS tests", environment)
+OSTests::OSTests(const TestNumber& number, const TestContext& context)
+    : TestSequence(number, "OS tests", context)
 {
     append<HeapAllocationErrorsTest>("ISHIKO_OS test 1", IshikoOSTest1);
     append<HeapAllocationErrorsTest>("ISHIKO_OS test 2", IshikoOSTest2);
--- tests/src/OSTests.hpp
+++ tests/src/OSTests.hpp
@@ -4,15 +4,15 @@
     See https://github.com/ishiko-cpp/base-platform/blob/main/LICENSE.txt
 */
 
-#ifndef _ISHIKO_CPP_BASEPLATFORM_TESTS_OSTESTS_H_
-#define _ISHIKO_CPP_BASEPLATFORM_TESTS_OSTESTS_H_
+#ifndef _ISHIKO_CPP_BASEPLATFORM_TESTS_OSTESTS_HPP_
+#define _ISHIKO_CPP_BASEPLATFORM_TESTS_OSTESTS_HPP_
 
 #include <Ishiko/Tests/Core.hpp>
 
 class OSTests : public Ishiko::Tests::TestSequence
 {
 public:
-    OSTests(const Ishiko::Tests::TestNumber& number, const Ishiko::Tests::TestEnvironment& environment);
+    OSTests(const Ishiko::Tests::TestNumber& number, const Ishiko::Tests::TestContext& context);
 
 private:
     static void IshikoOSTest1(Ishiko::Tests::Test& test);
