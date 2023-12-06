623570d4fe47023893b1340370272a659aa419a2
Xavier Leclercq <xavier@leclercq.dev>
2023-12-06 21:51:46 +0000

    Fix tests compilation (#73)

--- tests/src/OSTests.cpp
+++ tests/src/OSTests.cpp
@@ -1,11 +1,11 @@
 /*
-    Copyright (c) 2020-2022 Xavier Leclercq
+    Copyright (c) 2020-2023 Xavier Leclercq
     Released under the MIT License
     See https://github.com/ishiko-cpp/base-platform/blob/main/LICENSE.txt
 */
 
 #include "OSTests.hpp"
-#include "Ishiko/BasePlatform/OS.h"
+#include "Ishiko/BasePlatform/OS.hpp"
 
 using namespace Ishiko;
 
