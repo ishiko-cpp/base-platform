186c44a31a2ef7d289e4435581a6a685a6ccad75
Xavier Leclercq <xavier@leclercq.dev>
2023-12-06 20:55:28 +0000

    Update IshikoWindows.hpp (#71)

--- include/Ishiko/BasePlatform/windows/IshikoWindows.hpp
+++ include/Ishiko/BasePlatform/windows/IshikoWindows.hpp
@@ -7,12 +7,18 @@
 #ifndef GUARD_ISHIKO_CPP_BASEPLATFORM_WINDOWS_ISHIKOWINDOWS_HPP
 #define GUARD_ISHIKO_CPP_BASEPLATFORM_WINDOWS_ISHIKOWINDOWS_HPP
 
-#ifdef WIN32_LEAN_AND_MEAN
-    #include <Windows.h>
-#else
-    #define WIN32_LEAN_AND_MEAN
-    #include <Windows.h>
-    #undef WIN32_LEAN_AND_MEAN
-#endif
+#pragma push_macro("UNICODE")
+#pragma push_macro("WIN32_LEAN_AND_MEAN")
+
+// There seems to be an error with the definition of GetEnvironmentStringsA so we undefine UNICODE so that we can use
+// GetEnvironmentStrings. To get the ASCII version call GetEnvironmentStrings, to get the Unicode version call 
+// GetEnvironmentStringsW.
+#undef UNICODE
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+#pragma pop_macro("UNICODE")
+#pragma pop_macro("WIN32_LEAN_AND_MEAN")
 
 #endif
