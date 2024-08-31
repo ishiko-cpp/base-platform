14669c568e1d0e8cc53eab635a743b92431269b6
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2024-08-31 18:53:06 +0100

    Rename file

--- include/Ishiko/BasePlatform.hpp
+++ include/Ishiko/BasePlatform.hpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2020-2023 Xavier Leclercq
+    Copyright (c) 2020-2024 Xavier Leclercq
     Released under the MIT License
     See https://github.com/ishiko-cpp/base-platform/blob/main/LICENSE.txt
 */
@@ -7,7 +7,7 @@
 #ifndef GUARD_ISHIKO_CPP_BASEPLATFORM_HPP
 #define GUARD_ISHIKO_CPP_BASEPLATFORM_HPP
 
-#include "BasePlatform/Compilers.hpp"
+#include "BasePlatform/Compiler.hpp"
 #include "BasePlatform/linkoptions.hpp"
 #include "BasePlatform/OS.hpp"
 
