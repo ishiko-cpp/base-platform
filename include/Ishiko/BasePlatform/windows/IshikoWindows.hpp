cdc4b859f4c184a128242152a9808dc2ce26bec7
Xavier Leclercq <xavier@leclercq.dev>
2025-12-31 15:19:25 +0000

    Remove min and max macros from Windows.h

--- include/Ishiko/BasePlatform/windows/IshikoWindows.hpp
+++ include/Ishiko/BasePlatform/windows/IshikoWindows.hpp
@@ -1,11 +1,15 @@
-// SPDX-FileCopyrightText: 2000-2024 Xavier Leclercq
+// SPDX-FileCopyrightText: 2000-2025 Xavier Leclercq
 // SPDX-License-Identifier: BSL-1.0
 
 #ifndef GUARD_ISHIKO_CPP_BASEPLATFORM_WINDOWS_ISHIKOWINDOWS_HPP
 #define GUARD_ISHIKO_CPP_BASEPLATFORM_WINDOWS_ISHIKOWINDOWS_HPP
 
+#pragma push_macro("NOMINMAX")
 #pragma push_macro("UNICODE")
 #pragma push_macro("WIN32_LEAN_AND_MEAN")
+
+// Remove the min and max macro because they create conflicts.
+#define NOMINMAX
 
 // There seems to be an error with the definition of GetEnvironmentStringsA so we undefine UNICODE so that we can use
 // GetEnvironmentStrings. To get the ASCII version call GetEnvironmentStrings, to get the Unicode version call 
@@ -15,6 +19,7 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
+#pragma pop_macro("NOMINMAX")
 #pragma pop_macro("UNICODE")
 #pragma pop_macro("WIN32_LEAN_AND_MEAN")
 
